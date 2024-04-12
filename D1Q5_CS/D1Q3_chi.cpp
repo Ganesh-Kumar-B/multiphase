35d5cf56019375d304508cb748e710a3a49b9200
Ganesh B <ganeshkumar951999@gmail.com>
2024-04-12 12:37:09 +0530

    equilibrium rewritten

--- D1Q5_CS/D1Q3_chi.cpp
+++ D1Q5_CS/D1Q3_chi.cpp
@@ -1,5 +1,5 @@
 /*
-  A Simple 1D D1Q3 code for MultiPhase flow
+  A Simple 1D D1Q5 code for MultiPhase flow
  */
 #include <stdio.h>
 #include <iostream>
@@ -20,15 +20,13 @@ enum direction
     DM3X, /* Alias for -3 */
 };
 
-typedef  struct nodeD1Q3
+typedef  struct nodeD1Q5
 {
     double rho;
     double rhoOld;
     double pNid;
     double FNid;
     double chi;
-    double chiPredictor;
-    double chiCorrector;
     double muR;
     double muA;
     double dmuA;
@@ -49,20 +47,20 @@ typedef  struct nodeD1Q3
     double f0[N_DV]; 
     double Fh[N_DV]; 
     double df[N_DV];
-} nodeD1Q3;
+} nodeD1Q5;
 
-typedef  nodeD1Q3 latticeArr[MAX_SIZE]  ;
+typedef  nodeD1Q5 latticeArr[MAX_SIZE]  ;
  
-typedef  struct latticeD1Q3
+typedef  struct latticeD1Q5
 {
     double weight[N_DV];
-    double dvD1Q3[N_DV];
+    double dvD1Q5[N_DV];
     int sgn[N_DV];
     double T0;
     double T0Inv;
     double c;
     double c2;
-} latticeD1Q3;
+} latticeD1Q5;
 
 
 typedef  struct nonIdealParam  
@@ -74,33 +72,33 @@ typedef  struct nonIdealParam
     double rho0;
 } nonIdealParam ;
 
-void getLatticeD1Q3(double c, latticeD1Q3 *myD1Q3);
-void printRho(latticeArr lattice, latticeD1Q3 myD1Q3, nonIdealParam myVDW,int nX, double beta,int iX_begin, int iX_end, int time,double c);
-void getFeqPQuad(double fEq[N_DV], latticeD1Q3 myD1Q3,  double rho,  double vel);
+void getLatticeD1Q5(double c, latticeD1Q5 *myD1Q5);
+void printRho(latticeArr lattice, latticeD1Q5 myD1Q5, nonIdealParam myVDW,int nX, double beta,int iX_begin, int iX_end, int time,double c);
+void getFeqPQuad(double fEq[N_DV], latticeD1Q5 myD1Q5,  double rho,  double vel);
 
 void createBoundaryPeriodic(latticeArr lattice, int nX );
-void collideWorking (latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVDW, int nX, double beta,double tau, int time,double c);
+void collideWorking (latticeArr myLattice, latticeD1Q5 myD1Q5, nonIdealParam myVDW, int nX, double beta,double tau, int time,double c);
 void advect(latticeArr lattice, int nX);
 void calculateAlpha( latticeArr myLattice, double& alpha, int i, int time);
 void calculateAlpha1( latticeArr myLattice, double& alpha, int i, int time, double beta);
 
-void initializePerturbPeriodic(latticeArr lattice, latticeD1Q3 myD1Q3,  int nX, double vel, double rhoMean, double ampDisturb, double periodDisturb);
+void initializePerturbPeriodic(latticeArr lattice, latticeD1Q5 myD1Q5,  int nX, double vel, double rhoMean, double ampDisturb, double periodDisturb);
 
 
 main()
 {
   
     latticeArr myLattice;
-	latticeD1Q3 myD1Q3;
+	latticeD1Q5 myD1Q5;
 	double kappabar, TbyTc,rho0byrhoc;
 	double c, rho,u, beta,  T_critical, tau, dt, dX, rho_critical;
 	nonIdealParam myVDW;
 	int iX,nX,time, finalTime;
 	c = 1;
-	getLatticeD1Q3(c, &myD1Q3);
+	getLatticeD1Q5(c, &myD1Q5);
  
 
-
+ 
     nX = 500;
 	beta = 0.6;
 	TbyTc = 0.98;
@@ -112,7 +110,7 @@ main()
 	tau = (1.0-beta)/beta *dt*0.5;   
 	printf("\n beta=%lf kn =%lf \n",beta, tau);
 	
-    T_critical = myD1Q3.T0/TbyTc ;
+    T_critical = myD1Q5.T0/TbyTc ;
     rho_critical = 1.0;
 
     myVDW.b = 0.521772/(rho_critical);
@@ -123,7 +121,7 @@ main()
 
     finalTime = 990000    ;
 
-	initializePerturbPeriodic(  myLattice,   myD1Q3,    nX, 0.0, myVDW.rho0, 0.001,2 );//0.001,2
+	initializePerturbPeriodic(  myLattice,   myD1Q5,    nX, 0.0, myVDW.rho0, 0.001,2 );//0.001,2
 	createBoundaryPeriodic(  myLattice,  nX );
     
 	for(time =0; time<= finalTime ; time++)
@@ -131,7 +129,7 @@ main()
        for( int iX = nX+2  ; iX >=3 ; iX--) 
        {
           myLattice[iX].rho = myLattice[iX].f[ZERO] + myLattice[iX].f[DX] + myLattice[iX].f[DMX] + myLattice[iX].f[D3X] + myLattice[iX].f[DM3X];
-//           myLattice[iX].vel = (myD1Q3.dvD1Q3[DX]*(myLattice[iX].f[DX]-myLattice[iX].f[DMX]))/myLattice[iX].rho ; 
+//           myLattice[iX].vel = (myD1Q5.dvD1Q5[DX]*(myLattice[iX].f[DX]-myLattice[iX].f[DMX]))/myLattice[iX].rho ; 
         //   double Force_i    = -(myLattice[iX+1].pNid - myLattice[iX-1].pNid)/(dX*2.0);
         //   myLattice[iX].vel = (myLattice[iX].vel + 0.5*dt*Force_i)/myLattice[iX].rho ; //the velocity is actually not needed this thing is for computing the kappa term gradient
        }
@@ -152,48 +150,47 @@ main()
         
           
         
-	   collideWorking (  myLattice,   myD1Q3, myVDW,   nX,  beta,tau,time,c);  
+	   collideWorking (  myLattice,   myD1Q5, myVDW,   nX,  beta,tau,time,c);  
        
 	   createBoundaryPeriodic ( myLattice,  nX );
 	   advect(   myLattice,       nX);
 
        if(time % 9900== 0) {
-         printRho(myLattice,myD1Q3,myVDW,nX,beta,3,nX+2,time,c);
+         printRho(myLattice,myD1Q5,myVDW,nX,beta,3,nX+2,time,c);
        }
 /*  ______________________________________________________________   */	  
 	}
-    printRho(myLattice,myD1Q3,myVDW,nX,beta,3,nX+2,finalTime,c);
+    printRho(myLattice,myD1Q5,myVDW,nX,beta,3,nX+2,finalTime,c);
 }
 
 
-void   getLatticeD1Q3(double c, latticeD1Q3 *myD1Q3)
+void   getLatticeD1Q5(double c, latticeD1Q5 *myD1Q5)
 {
-        double tmp;
-		tmp =sqrt(10.0);
-		myD1Q3->T0 = c*c/( (5.0/3.0) + (sqrt(10) /3.0)  );
-		printf("\nTemp = %lf\n", myD1Q3->T0);
-		myD1Q3->T0Inv = 1.0/myD1Q3->T0;
-		myD1Q3->c = c; 
-	   	myD1Q3->c2 = c*c;
+        
+		myD1Q5->T0 = c*c/( (5.0/3.0) + (sqrt(10) /3.0)  );
+		printf("\nTemp = %lf\n", myD1Q5->T0);
+		myD1Q5->T0Inv = 1.0/myD1Q5->T0;
+		myD1Q5->c = c; 
+	   	myD1Q5->c2 = c*c;
 
-		myD1Q3->weight[ZERO] =  (1.0/720.0)*( 64.0*(4.0+ sqrt(10)))    ;
-		myD1Q3->weight[DX]   =  myD1Q3->weight[DMX]  = (1.0/720.0)*( 27.0*(8.0 - sqrt(10)))     ;
-        myD1Q3->weight[D3X]  =  myD1Q3->weight[DM3X] = (1.0/720.0)*( 1.0*(16.0 - 5*sqrt(10)))    ;
-
+		myD1Q5->weight[ZERO] =  (1.0/720.0)*( 64.0*(4.0+ sqrt(10)))    ;
+		myD1Q5->weight[DX]   =  myD1Q5->weight[DMX]  = (1.0/720.0)*( 27.0*(8.0 - sqrt(10)))     ;
+        myD1Q5->weight[D3X]  =  myD1Q5->weight[DM3X] = (1.0/720.0)*( 1.0*(16.0 - 5*sqrt(10)))    ;
 
 
-		myD1Q3->dvD1Q3[ZERO] = 0.0;
-		myD1Q3->dvD1Q3[DX]   = c;
-		myD1Q3->dvD1Q3[DMX]  = -1.0 * c;
-		myD1Q3->dvD1Q3[D3X]   = 3.0*c;
-		myD1Q3->dvD1Q3[DM3X]  = -3.0 * c;
-		myD1Q3->sgn[ZERO] = 0;
-		myD1Q3->sgn[DX]   = 1;
-		myD1Q3->sgn[DMX]  = -1 ;
-		myD1Q3->sgn[D3X]   = 1;
-		myD1Q3->sgn[DM3X]  = -1;
-		printf("\n w0=%lf w1= %lf  \n", myD1Q3->weight[ZERO], myD1Q3->weight[DX]);
-		printf("\n rho=%.16lf  T0=%.16lf\n", myD1Q3->weight[ZERO]+2.0*(myD1Q3->weight[DX] + myD1Q3->weight[DM3X]),myD1Q3->T0 );
+
+		myD1Q5->dvD1Q5[ZERO] = 0.0;
+		myD1Q5->dvD1Q5[DX]   = c;
+		myD1Q5->dvD1Q5[DMX]  = -1.0 * c;
+		myD1Q5->dvD1Q5[D3X]   = 3.0*c;
+		myD1Q5->dvD1Q5[DM3X]  = -3.0 * c;
+		myD1Q5->sgn[ZERO] = 0;
+		myD1Q5->sgn[DX]   = 1;
+		myD1Q5->sgn[DMX]  = -1 ;
+		myD1Q5->sgn[D3X]   = 1;
+		myD1Q5->sgn[DM3X]  = -1;
+		printf("\n w0=%lf w1= %lf  \n", myD1Q5->weight[ZERO], myD1Q5->weight[DX]);
+		printf("\n rho=%.16lf  T0=%.16lf\n", myD1Q5->weight[ZERO]+2.0*(myD1Q5->weight[DX] + myD1Q5->weight[DM3X]),myD1Q5->T0 );
 		return;
 }
 		
@@ -245,7 +242,7 @@ void advect(latticeArr myLattice, int nX)
     return;
 } 
 
-void initializePerturbPeriodic(latticeArr myLattice, latticeD1Q3 myD1Q3,  int nX, double vel, double rhoMean, double ampDisturb, double periodDisturb)
+void initializePerturbPeriodic(latticeArr myLattice, latticeD1Q5 myD1Q5,  int nX, double vel, double rhoMean, double ampDisturb, double periodDisturb)
 {
     int iX;
     double coord, rhoIn, kx;
@@ -259,14 +256,14 @@ void initializePerturbPeriodic(latticeArr myLattice, latticeD1Q3 myD1Q3,  int nX
 	    rhoIn =rhoMean + ampDisturb*sin(kx*coord);
         myLattice[iX].rho =rhoIn;
         /*Inititalise f using equilibrium*/
-        getFeqPQuad(myLattice[iX].f,myD1Q3, rhoIn, vel);
+        getFeqPQuad(myLattice[iX].f,myD1Q5, rhoIn, vel);
         fprintf(fpt,"%.10lf  %.10lf \n", coord, rhoIn);
     }
     createBoundaryPeriodic(  myLattice,   nX );
     fclose(fpt);
  }
 
-void printRho(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVDW, int nX, double beta,int iX_begin, int iX_end, int time, double c)
+void printRho(latticeArr myLattice, latticeD1Q5 myD1Q5, nonIdealParam myVDW, int nX, double beta,int iX_begin, int iX_end, int time, double c)
 {
     FILE *fpt;
     int iX;
@@ -289,28 +286,30 @@ void printRho(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVDW, int
     {
         double P = 0.0, mu = 0.0, dx=0.0, temp;
         dx = 1.0/(nX-1.0);
+        double eta_EOS = myLattice[iX].rho * myVDW.b / 4.0;
     
-        P = myLattice[iX].rho*myD1Q3.T0/(1.0-myLattice[iX].rho*myVDW.b);
-        P += -myVDW.a*myLattice[iX].rho*myLattice[iX].rho;
-        P += -myVDW.kappa*myLattice[iX].rho*(myLattice[iX+1].rho+myLattice[iX-1].rho-2.0*myLattice[iX].rho)/(dx*dx);
+        P =(myLattice[iX].rho*myD1Q5.T0  ) *(1+eta_EOS + eta_EOS*eta_EOS - pow(eta_EOS, 3) )/pow((1.0 - eta_EOS), 3)
+                            - myVDW.a * myLattice[iX].rho*myLattice[iX].rho 
+                            - myLattice[iX].rho*myD1Q5.T0
+                            ;
         P += -0.5*myVDW.kappa*(myLattice[iX+1].rho - myLattice[iX-1].rho)*(myLattice[iX+1].rho - myLattice[iX-1].rho)/(4.0*dx*dx);
         //correction
         //P += -(0.5/beta - 0.25)*temp*temp/myLattice[iX].rho; //term hurts
         //P += 0.25*myVDW.kappa*(myLattice[iX+1].P+myLattice[iX-1].P-2.0*myLattice[iX].P)/(dx*dx); //term may be fine
         //P += -1.0/12.0*myVDW.kappa*(myLattice[iX+1].rho+myLattice[iX-1].rho-2.0*myLattice[iX].rho)/(dx*dx); // this term is good
 
-        mu = myD1Q3.T0*log(myLattice[iX].rho) ;
-        mu += -1.5*myD1Q3.T0*log(2.0*M_PI*myD1Q3.T0);
-        mu += -2.0*myVDW.a*myLattice[iX].rho -myD1Q3.T0*log(1.0-myLattice[iX].rho*myVDW.b);
-        mu += myLattice[iX].rho*myD1Q3.T0*myVDW.b/(1.0-myLattice[iX].rho*myVDW.b);
+        mu = myD1Q5.T0*log(myLattice[iX].rho) ;
+        mu += -1.5*myD1Q5.T0*log(2.0*M_PI*myD1Q5.T0);
+        mu += -2.0*myVDW.a*myLattice[iX].rho -myD1Q5.T0*log(1.0-myLattice[iX].rho*myVDW.b);
+        mu += myLattice[iX].rho*myD1Q5.T0*myVDW.b/(1.0-myLattice[iX].rho*myVDW.b);
         mu += - myVDW.kappa*(myLattice[iX-1].rho + myLattice[iX+1].rho - 2.0*myLattice[iX].rho)/(dx*dx);
 
-        fprintf(fpt,  "%d %.7lf  %.7lf %.7lf  %.7lf  %.7lf \n",iX, myLattice[iX].rho, myLattice[iX].vel, P, mu,myLattice[iX].chi);
+        fprintf(fpt,  "%d,%.7lf,%.7lf,%.7lf,%.7lf,%.7lf\n",iX, myLattice[iX].rho, myLattice[iX].vel, P, mu,myLattice[iX].chi);
     }
     fclose(fpt);
 }
 
-void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVDW,  int nX, double beta,double tau, int time, double c)
+void collideWorking(latticeArr myLattice, latticeD1Q5 myD1Q5, nonIdealParam myVDW,  int nX, double beta,double tau, int time, double c)
 {
     int iX,dv;
     double  fact,quad, quad1, tmp;
@@ -324,8 +323,8 @@ void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVD
 //    	//define pressure
 //     for( iX = nX+2 ; iX >=3 ; iX--)
 //     {
-// //         myLattice[iX].pNid = myVDW.b*myLattice[iX].rho*myLattice[iX].rho*myD1Q3.T0/(1-myVDW.b*myLattice[iX].rho) - myLattice[iX].rho*myLattice[iX].rho*myVDW.a ;
-//        myLattice[iX].pNid = myLattice[iX].chi*myLattice[iX].rho*myD1Q3.T0 ;
+// //         myLattice[iX].pNid = myVDW.b*myLattice[iX].rho*myLattice[iX].rho*myD1Q5.T0/(1-myVDW.b*myLattice[iX].rho) - myLattice[iX].rho*myLattice[iX].rho*myVDW.a ;
+//        myLattice[iX].pNid = myLattice[iX].chi*myLattice[iX].rho*myD1Q5.T0 ;
 //         //surface energy
 //     } 
 
@@ -333,9 +332,9 @@ void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVD
         for( iX = nX+2 ; iX >=3 ; iX--)
     {
         double eta_EOS = myLattice[iX].rho * myVDW.b / 4.0;
-        myLattice[iX].pNid = (myLattice[iX].rho*myD1Q3.T0  ) *(1+eta_EOS + eta_EOS*eta_EOS - pow(eta_EOS, 3) )/pow((1.0 - eta_EOS), 3)
+        myLattice[iX].pNid = (myLattice[iX].rho*myD1Q5.T0  ) *(1+eta_EOS + eta_EOS*eta_EOS - pow(eta_EOS, 3) )/pow((1.0 - eta_EOS), 3)
                             - myVDW.a * myLattice[iX].rho*myLattice[iX].rho 
-                            - myLattice[iX].rho*myD1Q3.T0
+                            - myLattice[iX].rho*myD1Q5.T0
                             ;
                   // pnid =  (rho**2 b theta) / (1 - rho * b) - a rho**2 - rho theta
     }
@@ -344,7 +343,7 @@ void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVD
         for( iX = nX+2 ; iX >=3 ; iX--)
     {   
         double eta_EOS = myLattice[iX].rho * myVDW.b / 4.0;
-        myLattice[iX].FNid =  - myVDW.a * myLattice[iX].rho*myLattice[iX].rho   -   myLattice[iX].rho*myD1Q3.T0 *(3.0 * eta_EOS*eta_EOS - 4.0*eta_EOS) / pow(1 - eta_EOS,2)
+        myLattice[iX].FNid =  - myVDW.a * myLattice[iX].rho*myLattice[iX].rho   -   myLattice[iX].rho*myD1Q5.T0 *(3.0 * eta_EOS*eta_EOS - 4.0*eta_EOS) / pow(1 - eta_EOS,2)
                             - myVDW.kappa * 0.5*pow((myLattice[iX+1].rho -myLattice[iX-1].rho)/(dx*2.0),2)
         ;
     }
@@ -369,10 +368,21 @@ void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVD
         //from continuous derivative
         double eta_EOS = myLattice[iX].rho * myVDW.b / 4.0;
 
-        myLattice[iX].muA = myD1Q3.T0*(3.0*pow(eta_EOS,3) - 9.0 *eta_EOS*eta_EOS + 8.0 *eta_EOS )/(pow(1 - eta_EOS,3));
+        myLattice[iX].muA = myD1Q5.T0*(3.0*pow(eta_EOS,3) - 9.0 *eta_EOS*eta_EOS + 8.0 *eta_EOS )/(pow(1 - eta_EOS,3));
         myLattice[iX].muA -= 2.0*myLattice[iX].rho*myVDW.a ;
 
         myLattice[iX].muA -= myVDW.kappa*(myLattice[iX-1].rho + myLattice[iX+1].rho - 2.0*myLattice[iX].rho)/(dx*dx) ;
+
+        double kappa1 = myVDW.kappa;
+
+        // // # correction from chemical potential
+        // kappa1 =   myVDW.kappa 
+        //             -   (dx*dx/6.0)    *(   
+        //                             )
+        //             ;
+
+        myLattice[iX].muA -= kappa1*(myLattice[iX-1].rho + myLattice[iX+1].rho - 2.0*myLattice[iX].rho)/(dx*dx) ;
+
 
     }
 	myLattice[0].muA = myLattice[nX].muA    ;
@@ -440,7 +450,7 @@ void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVD
         //                 ;
         // myLattice[iX].Force = myLattice[iX].rho*(del_muA_second_order + del_muA_fourth_order); //total force
 
-//   //#fourth order to second order smoothly
+//   //#fourth order to second order 
     // // a = 1 represents 2nd order a = 4/3 represents 4th order
         double a = 3.0/3.0; double b = 1.0 - a;
         double del_muA_fourth_order =   (myLattice[iX  ].pNid  + myLattice[iX  ].FNid)*
@@ -463,13 +473,12 @@ void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVD
 
         myLattice[iX].Force = -myLattice[iX].Force /myLattice[iX].rho; //force density
 
-        myLattice[iX].vel = (myD1Q3.dvD1Q3[DX]*(myLattice[iX].f[DX]-myLattice[iX].f[DMX]) + 3*myLattice[iX].f[D3X] - 3*myLattice[iX].f[DM3X]  ) ;
+        myLattice[iX].vel = (myD1Q5.dvD1Q5[DX]*(myLattice[iX].f[DX]-myLattice[iX].f[DMX]) + 3*myLattice[iX].f[D3X] - 3*myLattice[iX].f[DM3X]  ) ;
 
         myLattice[iX].vel = myLattice[iX].vel/myLattice[iX].rho +  0.5*dt*(myLattice[iX].Force);
         
-        getFeqPQuad( myLattice[iX].fEq ,   myD1Q3,    myLattice[iX].rho,   myLattice[iX].vel); 
+        getFeqPQuad( myLattice[iX].fEq ,   myD1Q5,    myLattice[iX].rho,   myLattice[iX].vel); 
     } 
-
     for( iX = nX+2  ; iX >=3 ; iX--)
     {
 	    alpha = 2.0;
@@ -479,7 +488,7 @@ void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVD
 	    double chi = 0.0;
 
         rhoReduced = myVDW.b*myLattice[iX].rho;
-        chi = rhoReduced/(1.0-rhoReduced) - myVDW.a*myLattice[iX].rho/myD1Q3.T0;
+        chi = rhoReduced/(1.0-rhoReduced) - myVDW.a*myLattice[iX].rho/myD1Q5.T0;
         tauM = tau;//*myVDW.rho0/myLattice[iX].rho *(1 + rhoReduced*(5.0/8.0 + rhoReduced*(0.2869+ rhoReduced*(0.1103+ 0.0386*rhoReduced))));
 //             tauM = tauM/(1.0+chi);
 	    betaM = dt/(2.0*tauM + dt);
@@ -487,7 +496,7 @@ void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVD
         for(dv = 0; dv < N_DV; dv++)
         {
             myLattice[iX].f[dv]  = myLattice[iX] .f[dv] + alpha*betaM*(myLattice[iX].fEq[dv] - myLattice[iX] .f[dv]); 
-            myLattice[iX].f[dv] += alpha*betaM*tauM*myD1Q3.T0Inv*myLattice[iX].rho*myD1Q3.weight[dv]*myD1Q3.dvD1Q3[dv]*myLattice[iX].Force;
+            myLattice[iX].f[dv] += alpha*betaM*tauM*myD1Q5.T0Inv*myLattice[iX].rho*myD1Q5.weight[dv]*myD1Q5.dvD1Q5[dv]*myLattice[iX].Force;
         }      
     }
       
@@ -495,32 +504,31 @@ void collideWorking(latticeArr myLattice, latticeD1Q3 myD1Q3, nonIdealParam myVD
     printf("\nAt time =%d mass = %.16lf ", time, mass);
 }
 
-void getFeqPQuad(double fEq[N_DV], latticeD1Q3 myD1Q3,  double rho,  double vel)
+void getFeqPQuad(double fEq[N_DV], latticeD1Q5 myD1Q5,  double rho,  double vel)
 {
    
-    double  fact, fact3,quad, quad1, tmp,tmp3, cubic;
-    double  ratio, ratio3, quad3, cubic1, vel1;
-    ratio = myD1Q3.T0Inv*myD1Q3.c2;
-    ratio3 =  myD1Q3.T0Inv*myD1Q3.c2 *3*3;
-    quad = myD1Q3.T0Inv *0.5*  vel*vel ;
-  
-    fEq[ZERO]= myD1Q3.weight[ZERO] *rho*  (1.0  -  quad) ;
+   double u2 = vel*vel ;
 
-    fact    =  myD1Q3.T0Inv * vel*myD1Q3.dvD1Q3[DX];
-    fact3   =  myD1Q3.T0Inv * vel*myD1Q3.dvD1Q3[D3X];
+    double first,second, third,feq0=0;
 
-    quad1  = quad *(ratio  -1.0); 
-    quad3  = quad *(ratio3 -1.0);
-    tmp = rho* myD1Q3.weight[DX];
-    tmp3 = rho* myD1Q3.weight[D3X];
+    for (int dv = 0; dv< 5; dv++){
 
-    fEq[DX]= tmp*(1.0 + fact + quad1   ); 
-    fEq[DMX]= tmp*(1.0 - fact + quad1 ); 
-  
-    fEq[D3X]= tmp3*(1.0 + fact3 + quad3   ); 
-    fEq[DM3X]= tmp3*(1.0 - fact3 + quad3   ); 
+        feq0 = rho*myD1Q5.weight[dv];
+
+        first  = (vel*myD1Q5.dvD1Q5[dv])*myD1Q5.T0Inv;
+
+        second = 0.5*(first * first);
+        third = -0.5*u2*myD1Q5.T0Inv ;
+
+        fEq[dv] = feq0*(1+ first + second + third);    
+
+        
+    }
+
+
     return;
 }
+
 
 
 void calculateAlpha1( latticeArr myLattice, double& alpha, int i,int , double beta)
